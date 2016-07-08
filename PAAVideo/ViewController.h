bfa829967ca6bfb51b1676f728b523e7b243c241
ChenZhiWu <1812586173@qq.com>
2016-07-08 17:17:45 +0800

    Initial Commit

---
+++ PAAVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PAAVideo
+//
+//  Created by 陈志武 on 16/7/8.
+//  Copyright © 2016年 陈志武. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PAAVideo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PAAVideo
+//
+//  Created by 陈志武 on 16/7/8.
+//  Copyright © 2016年 陈志武. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
